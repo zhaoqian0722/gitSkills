c0762f2f227d0bcb8de7518130cc80e85261e009
zhaoqian0722 <852113606@qq.com>
2018-11-03 00:51:24 +0800

    测试完成，删除无用的

--- Test/src/com/qiange/text/main/TestMain.java
+++
@@ -1,9 +0,0 @@
-package com.qiange.text.main;
-
-import java.util.HashMap;
-
-public class TestMain {
-	public static void main(String[] args) {
-		HashMap<String, Object> map = new HashMap<String, Object>();
-	}
-}
